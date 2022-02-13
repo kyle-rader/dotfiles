52687bb46eef57fdacffdf22f0707132ae101917
Kyle W. Rader <kylewrader@gmail.com>
2022-02-13 10:43:34 -0800

    Actually install the features

--- setup-win.ps1
+++ setup-win.ps1
@@ -1,6 +1,12 @@
 Write-Host "Starting Setup..."
 
+Write-Host "Installing OpenSSH Features..."
 Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 $wingetApps = @(
     'Microsoft.Git'
