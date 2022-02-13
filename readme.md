ec882cb28925921a79649b6a0864224f77ea1443
Kyle W. Rader <kylewrader@gmail.com>
2022-02-12 19:36:52 -0800

    Add function to download win-get

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Kyle's dotfiles and setup scripts
+
+## Windows
+In 2022, we can now have a good dev experience on Windows!
+
+```powershell
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/kyle-rader/dotfiles/master/setup-win.ps1'))
+```
--- setup-win.ps1
+++ setup-win.ps1
@@ -1,3 +1,36 @@
-Write-Host "Starting Setup..."
+function Download-Winget {
+    $repo = "microsoft/winget-cli"
+    $file = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 
+    $releases = "https://api.github.com/repos/$repo/releases"
+
+    Write-Host Determining latest release
+    $tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+
+    $download = "https://github.com/$repo/releases/download/$tag/$file"
+    $name = $file.Split(".")[0]
+    $zip = "$name-$tag.zip"
+    $dir = "$name-$tag"
+
+    Write-Host Dowloading latest release
+    Invoke-WebRequest $download -Out $zip
+
+    Write-Host Extracting release files
+    Expand-Archive $zip -Force
+
+    # Cleaning up target dir
+    Remove-Item $name -Recurse -Force -ErrorAction SilentlyContinue 
+
+    # Moving from temp dir to target dir
+    Move-Item $dir\$name -Destination $name -Force
+
+    # Removing temp files
+    Remove-Item $zip -Force
+    Remove-Item $dir -Recurse -Force
+}
+
+Write-Host "Starting Setup..."
+Download-Winget()
+
+# iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Write-Host "Setup Done."
