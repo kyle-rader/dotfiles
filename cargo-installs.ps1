0182ff5035b4044b41517fe5fc6922c7f70804bb
Kyle W. Rader <kylewrader@gmail.com>
2022-02-13 00:46:48 -0800

    Add dev tools to cargo install script

---
+++ cargo-installs.ps1
@@ -0,0 +1,44 @@
+Write-Host "Starting cargo installs"
+
+Write-Host "Adding Rust Clippy"
+rustup component add clippy
+
+@webInstallApps = (
+    bat
+    comrak
+    ripgrep
+    fd
+    jq
+    yq
+    pandoc
+)
+
+foreach ($app in @webInstallApps) {
+    Write-Host "Installing $app"
+    iex (curl.exe -A "MS" "https://webinstall.dev/$app")
+}
+
+@cargoInstallApps = (
+    cargo-edit
+    fd-find
+    git-delta
+    hexly
+    hyperfine
+    pastel
+    sd
+    xh
+)
+
+foreach ($app in @cargoInstallApps) {
+    Write-Host "Installing $app"
+    cargo install "$app" --locked
+}
+
+@cargoNightlyInstallApps = (
+    dua-cli
+)
+
+foreach ($app in @cargoNightlyInstallApps) {
+    Write-Host "Installing $app"
+    cargo +nightly install "$app" --locked
+}
