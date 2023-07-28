a68c6b5579d16c9494c033d75868969c9f3762cd
Kyle W. Rader <kylewrader@gmail.com>
2023-07-27 22:57:52 -0700

    Use header function

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+function header() {
+  printf "\n======================================================================\n"
+  printf "$1"
+  printf "\n======================================================================\n"
+}
+
 set -e
 
-echo Starting setup...
+header "Starting setup..."
 
 # Update and Upgrade
-echo Updating and Upgrading...
+header "Updating and Upgrading..."
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install essentials
-echo Installing essentials...
+header "Installing essentials..."
 sudo apt-get install -y \
     build-essential \
     pkg-config \
@@ -26,21 +32,21 @@ sudo apt-get install -y \
     zsh \
 
 # Install latest git
-echo Installing git...
+header "Installing git..."
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install git
 
 # Install Rust
-echo Installing Rust...
+header "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
-echo Installing Rustup components...
+header "Installing Rustup components..."
 rustup toolchain install nightly
 
 # Cargo installs
-echo Installing dev tools from cargo...
+header "Installing dev tools from cargo..."
 apps=(loki-cli nu bat ripgrep fd-find git-delta xh hyperfine hexyl pastel)
 for app in "${apps[@]}"; do
     cargo install $app --locked
@@ -49,5 +55,5 @@ done
 cargo +nightly install dua-cli --locked
 
 # Copy dotfiles
-echo Copying dotfiles...
+header "Copying dotfiles..."
 curl -L -o ~/.gitconfig https://raw.githubusercontent.com/kyle-rader/dotfiles/main/.gitconfig
