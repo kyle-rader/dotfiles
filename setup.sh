4e9d521255e88b9570c1cc45a2bf163a61385374
Kyle W. Rader <kylewrader@gmail.com>
2023-07-28 22:40:26 -0700

    Install Github CLI

--- setup.sh
+++ setup.sh
@@ -53,6 +53,16 @@ fi
 
 footer_good
 
+# Install GitHub CLI
+header "Installing GitHub CLI"
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+
+footer_good
+
 # Install Rust
 if [ -z "$(command -v cargo)" ]; then
     header "Installing Rust"
